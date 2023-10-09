7185351081e29748667088e5c91e658c585c40b5
Charis Chakim <132967245+CharisChakim@users.noreply.github.com>
2023-10-09 14:55:09 +0700

    main.py
    sedikit revisi, lupa push

--- problem3/main.py
+++ problem3/main.py
@@ -1,7 +1,7 @@
 def prime_number(num):
     if num == 1:
         return 'Not Prime'
-    elif num > 3:
+    elif num >= 2:
         for i in range (2,num):
             if (num % i) == 0:
                 return 'Not Prime'
